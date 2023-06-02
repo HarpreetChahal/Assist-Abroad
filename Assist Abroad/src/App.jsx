ac82266ac6e5d25644fa987d67e668b768d90c7d
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-06-02 11:24:01 -0600

    Project initialized setup

---
+++ Assist Abroad/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './Components/Home/Home'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <Home/>
+      <Login/>
+      <Register/>
+    </div>
+  )
+}
+
+export default App
