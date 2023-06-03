96f733b17f3842a3ef8dd3e6f0ec4f698b83dff7
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-06-03 17:14:19 -0600

    half screen image added

--- Assist Abroad/src/Components/Login/Login.jsx
+++ Assist Abroad/src/Components/Login/Login.jsx
@@ -3,10 +3,14 @@ import './Login.css'
 import {Link} from 'react-router-dom'
 
 
-
-import loginImage from '../../Assets/LOGIN.png'
+//Assets
+import image from '../../Assets/LOGIN.png'
 import logo from '../../Assets/LOGO.png'
 
+//Icons 
+import {FcGoogle} from 'react-icons/Fc'
+import {MdEmail} from 'react-icons/Md'
+import {RiLockPasswordFill} from 'react-icons/Ri'
 
 
 const Login = () => {
@@ -16,7 +20,7 @@ const Login = () => {
 
 
                 <div className='imageDiv'>
-                    <img src={loginImage} alt="Login Image"></img>
+                    <img src={image} alt="Login Image"></img>
 
                     <div className='textDiv'>
                         <h2 className='title'>ASSIST ABROAD</h2>
@@ -34,14 +38,41 @@ const Login = () => {
                     <div className='headerDiv'>
                         <img src={logo} alt="Logo Image"></img>
                         <h2>Welcome Back</h2>
+                       
                     </div>
+                    
 
                     <form action='' className='form grid'>
                         <span> Authetication error display</span>
+
+
+                        <button className='google flex'>
+                        <FcGoogle className='icon'/>
+                            <span>Sign in with Google</span>
+
+                        </button>
+                        
                         <div className='inputDiv'>
                          <label htmlFor='username'>Username</label>
-                         <div className='input flex'></div>   
+                         <div className='input flex'>
+                         <MdEmail className='icon'/>
+                         <input type='text' id='email' placeholder='Enter your email id'></input>
+                            </div>   
                         </div>
+                        <div className='inputDiv'>
+                         <label htmlFor='password'>Password</label>
+                         <div className='input flex'>
+                         <RiLockPasswordFill className='icon'/>
+                         <input type='password' id='password' placeholder='Enter your password'></input>
+                            </div>   
+                        </div>
+
+                        <button type='submit' className='btn flex'>
+                            <span>Login</span>
+
+                        </button>
+
+                        <span className='forgotPassword'>Forgot your password? <a href="">Click here</a></span>
                     </form>
                 </div>
 
