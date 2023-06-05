b23ba59b7b979a79c178520f23313028cbed9287
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-06-05 11:59:54 -0600

    Few plugins installed

---
+++ Assist Abroad/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        pr: "#6D81FE"
+      }
+    },
+  },
+  plugins: [],
+}
