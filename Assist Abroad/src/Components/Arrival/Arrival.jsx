99539af3c192b6c6b0f20a4be17be1c29537cc6a
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-06-09 20:32:05 -0600

    Feedback modal created

--- Assist Abroad/src/Components/Arrival/Arrival.jsx
+++ Assist Abroad/src/Components/Arrival/Arrival.jsx
@@ -208,7 +208,151 @@ function Modal({open, setOpen}) {
   const [value4, setValue4] = React.useState(0);
 
   return (
-  
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all my-8 w-96 lg:w-full lg:max-w-2xl">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-xl lg:text-2xl w-full text-center font-semibold leading-6 text-gray-900"
+                      >
+                        Feedback on Services
+                      </Dialog.Title>
+                      <div className="my-8">
+                        <div className="flex items-center justify-between lg:grid grid-cols-2  gap-2 lg:gap-8">
+                          <p className="text-sm lg:text-xl text-gray-500">
+                            Airport Pickup
+                          </p>
+                          <div className="flex items-center  gap-4">
+                            <Rating
+                              name="simple-controlled"
+                              value={value}
+                              onChange={(event, newValue) => {
+                                setValue(newValue);
+                              }}
+                            />
+                            <p className="text-sm lg:text-xl text-gray-500">
+                              {value === 0 ? "Not Yet Rated" : `${value}/5`}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between lg:grid grid-cols-2 mt-4  gap-2 lg:gap-8">
+                          <p className="text-sm lg:text-xl text-gray-500">Hotel Stay</p>
+                          <div className="flex items-center  gap-4">
+                            <Rating
+                              name="simple-controlled"
+                              value={value1}
+                              onChange={(event, newValue) => {
+                                setValue1(newValue);
+                              }}
+                            />
+                            <p className="text-sm lg:text-xl text-gray-500">
+                              {value1 === 0 ? "Not Yet Rated" : `${value1}/5`}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between lg:grid grid-cols-2 mt-4 l gap-2 lg:gap-8">
+                          <p className="text-sm lg:text-xl text-gray-500">Bus Pass</p>
+                          <div className="flex items-center  gap-4">
+                            <Rating
+                              name="simple-controlled"
+                              value={value2}
+                              onChange={(event, newValue) => {
+                                setValue2(newValue);
+                              }}
+                            />
+                            <p className="text-sm lg:text-xl text-gray-500">
+                              {value2 === 0 ? "Not Yet Rated" : `${value2}/5`}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between lg:grid grid-cols-2 mt-4  gap-2 lg:gap-8">
+                          <p className="text-sm lg:text-xl text-gray-500">
+                            Government Id 
+                          </p>
+                          <div className="flex items-center  gap-4">
+                            <Rating
+                              name="simple-controlled"
+                              value={value3}
+                              onChange={(event, newValue) => {
+                                setValue3(newValue);
+                              }}
+                            />
+                            <p className="text-sm lg:text-xl text-gray-500">
+                              {value3 === 0 ? "Not Yet Rated" : `${value3}/5`}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex items-center justify-between lg:grid grid-cols-2 mt-4  gap-2 lg:gap-8">
+                          <p className="text-sm lg:text-xl text-gray-500">Health Card</p>
+                          <div className="flex items-center  gap-4">
+                            <Rating
+                              name="simple-controlled"
+                              value={value4}
+                              onChange={(event, newValue) => {
+                                setValue4(newValue);
+                              }}
+                            />
+                            <p className="text-sm lg:text-xl text-gray-500">
+                              {value4 === 0 ? "Not Yet Rated" : `${value4}/5`}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="mt-4">
+                          <p className="text-gray-500 text-left">Write your feedback here:</p>
+                          <textarea
+                            className="border outline-none rounded-md mt-2 bg-transparent w-full"
+                            name=""
+                            id=""
+                            cols="30"
+                            rows="5"
+                          ></textarea>
+                        </div>
+                        <div className="flex items-center justify-end">
+                        <button className="text-white mt-4 bg-pr px-7 py-2 rounded-md">
+                  Give Feedback
+                </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
 
