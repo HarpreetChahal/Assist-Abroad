8dcde2133341096c67421867542925621cd1d23f
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-06-10 13:31:57 -0600

    Added all the api routes

--- Assist Abroad/src/api/list.js
+++ Assist Abroad/src/api/list.js
@@ -1,76 +1,21 @@
 const apiList = {
     // Authentication APIs
     register: {
-      url: () => "auth/register",
+      url: () => "web/api/v1/auth/register",
       method: "post"
     },
     login: {
-      url: () => "auth/login",
-      method: "post"
-    },
-  
-    // Posts APIs
-    createPost: {
-      url: () => "post/add",
-      method: "post"
-    },
-    fetchPost: {
-      url: () => "post/findAll",
-      method: "post"
-    },
-  
-    // createComment
-    createComment: {
-      url: () => "comment/add",
-      method: "post"
-    },
-  
-    // upload file
-    upload: {
-      url: () => "upload",
-      method: "post"
-    },
-    likeDislike: {
-      url: () => `post`,
-      method: "post"
-    },
-  
-    // User
-    updateUser: {
-      url: (id) => `user/${id}`,
-      method: "put"
-    },
-    // Post
-    deletePost: {
-      url: (id) => `post/${id}`,
-      method: "delete"
-    },
-  
-    // Friends and suggestions
-    suggestions: {
-      url: () => "user/suggestions",
-      method: "post"
-    },
-    followFriend: {
-      url: () => "user/followFriend",
-      method: "post"
-    },
-    friends: {
-      url: () => "user/friends",
-      method: "post"
-    },
-    followers: {
-      url: () => "user/followers",
-      method: "post"
-    },
-    unFollowFriend:{
-      url: () => "user/unFollowFriend",
-      method: "post"
-    },
-    getUser: {
-      url: (id) => `user/${id}`,
-      method: "get"
-    }
+        url: () => "web/api/v1/auth/login",
+        method: "post"
+      },
+      getMembership: {
+        url: () => "web/api/v1/membership/",
+        method: "get"
+      },
+      getMaster: {
+        url: () => "web/api/v1/master/",
+        method: "post"
+      },
   };
   export default apiList;
   
