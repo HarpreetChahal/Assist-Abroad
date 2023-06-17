004fd622aaa033349c50ccf375a7eb167bfd04bb
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-06-17 16:08:09 -0600

    Added the users list for the applointments

--- src/Components/AgentHome/AgentHome.jsx
+++ src/Components/AgentHome/AgentHome.jsx
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useState,useEffect ,useContext} from "react";
 import Navbar from "../../layout/Navbar";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import agent from "/src/Assets/agent.png";
 import { Link } from "react-router-dom";
+import commonApi from "../../api/common";
+import { Context } from "../context/Context";
+
 const AgentHome = () => {
   const [active, setActive] = useState(false);
+  const [appointments,setAppointments]=useState([])
+  const { dispatch,user, isFetching } = useContext(Context);
+
+  useEffect(()=>{
+    const fetchAppointments=async ()=>{
+      let data={
+        query:{
+          agentId:user._id,status:!active?"In-progress":"Completed"
+        },
+        options:{
+        }
+      }
+      await commonApi({
+        action: "listTask",
+        data: data,
+        config: {
+          authToken: true,
+        },
+      })
+        .then(({ DATA = {}, MESSAGE }) => {
+          setAppointments(DATA.data)
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+    fetchAppointments()
+  },[active])
   return (
     <div className=" min-h-screen px-4 lg:px-0">
       <Navbar />
@@ -37,46 +68,23 @@ const AgentHome = () => {
               Sort <AiOutlineArrowDown className="w-5 h-5" />
             </button>
           </div>
-          <Link
-            to="/agent-task/1"
-            className="mt-10 border-2 rounded-xl flex items-center gap-10 p-9"
-          >
-            <img className="w-32 lg:w-auto" src={agent} alt="" />
-            <div>
-              <h1 className="text-3xl">SAMAR DAHIYA</h1>
-              <h1 className="text-lg mt-1">Member Since : 2020</h1>
-            </div>
-          </Link>
-          <Link
-            to="/agent-task/1"
-            className="mt-10 border-2 rounded-xl flex items-center gap-10 p-9"
-          >
-            <img className="w-32 lg:w-auto" src={agent} alt="" />
-            <div>
-              <h1 className="text-3xl">SAMAR DAHIYA</h1>
-              <h1 className="text-lg mt-1">Member Since : 2020</h1>
-            </div>
-          </Link>
-          <Link
-            to="/agent-task/1"
-            className="mt-10 border-2 rounded-xl flex items-center gap-10 p-9"
-          >
-            <img className="w-32 lg:w-auto" src={agent} alt="" />
-            <div>
-              <h1 className="text-3xl">SAMAR DAHIYA</h1>
-              <h1 className="text-lg mt-1">Member Since : 2020</h1>
-            </div>
-          </Link>
-          <Link
-            to="/agent-task/1"
-            className="mt-10 border-2 rounded-xl flex items-center gap-10 p-9"
-          >
-            <img className="w-32 lg:w-auto" src={agent} alt="" />
-            <div>
-              <h1 className="text-3xl">SAMAR DAHIYA</h1>
-              <h1 className="text-lg mt-1">Member Since : 2020</h1>
-            </div>
-          </Link>
+         {appointments.map((appointment,index)=>{
+return <div
+className="mt-10 border-2 rounded-xl flex items-center gap-10 p-9"
+key={index}
+>
+<img className="w-32 lg:w-auto" src={agent} alt="" />
+<div>
+  <h1 className="text-3xl">{appointment.userObj?.name?.firstName}</h1>
+  <h1 className="text-lg mt-1">Email : {appointment.userObj?.email}</h1>
+
+  <h1 className="text-lg mt-1">Phone : {appointment.userObj?.phone?.phone}</h1>
+
+  {/* <h1 className="text-lg mt-1">Member Since : 2020</h1> */}
+</div>
+</div>
+         }) }
+         
         </div>
       </div>
     </div>
